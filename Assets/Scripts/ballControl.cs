6bd8196b814f07f3979abc699462f4463d527a5a
RafliNST <rafli.ran3103@gmail.com>
2025-02-26 20:59:52 +0700

    add power slider and goal-circle

--- Assets/Scripts/ballControl.cs
+++ Assets/Scripts/ballControl.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Events;
 using System.Collections;
 
 public class BallControl : MonoBehaviour
@@ -15,6 +15,9 @@ public class BallControl : MonoBehaviour
     [SerializeField] private int _maxBounces;
     private int _currentBounces;
     private bool isMoving = false;
+
+    public UnityEvent<float> speedForceChange;
+    public UnityEvent ballReleased;
 
     private int CurrentBounces
     {
@@ -54,7 +57,10 @@ public class BallControl : MonoBehaviour
     private void HandlePowerCharge()
     {
         if (Input.GetKey(KeyCode.Space))
+        {
             speedForce += speedForceRatio * Time.deltaTime;
+            speedForceChange.Invoke(speedForce);
+        }
     }
 
     private void HandleMovement()
@@ -79,6 +85,8 @@ public class BallControl : MonoBehaviour
 
         float rot = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rot);
+        
+        ballReleased.Invoke();
     }
 
     private void SmoothStop()
@@ -115,5 +123,10 @@ public class BallControl : MonoBehaviour
     {
         pointer.transform.position = transform.position + Vector3.up * pointerGap;
         pointer.rotation = Quaternion.identity;
+    }
+
+    public float GetMaxSpeed()
+    {
+        return maxSpeed;
     }
 }
